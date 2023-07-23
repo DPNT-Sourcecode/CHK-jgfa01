7c0ee1f27016c675b763fd97778ebeabb13c5458
sbx_user1051 <sbx_user1051@169.254.89.61>
2023-07-23 11:59:43 +0000

    Sun Jul 23 11:59:43 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,9 +5,8 @@
 def checkout(skus: str) -> str:
     store_items = {"A": [50, 3, 130], "B": [30, 2, 45], "C": [20], "D": [15]}
     static_skus = ("A", "B", "C")
-    for i in skus:
-        if i not in static_skus:
-            return -1
+    if skus not in static_skus or None:
+        return -1
 
     skus_counter_dict = {}
     for sku in skus:
@@ -23,5 +22,6 @@ def checkout(skus: str) -> str:
             total_cost += sku_list[0]
 
     return total_cost
+
 
 
